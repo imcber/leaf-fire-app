29bac0cc7df207cd56dce62faa0fc0e89262bb47
IMCBER <imcber1@gmail.com>
2019-10-31 03:14:22 -0600

    im load an example

--- src/App.js
+++ src/App.js
@@ -2,25 +2,114 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FilterableProductTable extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      filterText : "",
+      stockOnly : false
+    };
+    this.textFilterChange = this.textFilterChange.bind(this);
+    this.checkedFilterChange = this.checkedFilterChange.bind(this);
+  }
+
+  textFilterChange(e){
+    this.setState({
+      filterText : e.target.value
+    });
+  }
+
+  checkedFilterChange(e){
+    this.setState({
+      stockOnly : e.target.checked
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={this.state.filterText}
+            onChange={this.textFilterChange}
+          />
+          <p>
+            <input
+              type="checkbox"
+              checked={this.state.stockOnly}
+              onChange={this.checkedFilterChange}
+            />
+            {' '}
+            Only show products in stock
+          </p>
+        </form>
+        <div>
+          <ProductTable products={this.props.products} textFilter={this.state.filterText} checkFilter={this.state.stockOnly}/>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+class ProductTable extends React.Component{
+  render(){
+    const listaProductos = [];
+    const listaCategory = {};
+    this.props.products.forEach((product) => {
+      if(!listaCategory[product.category])
+        listaCategory[product.category] = [];
+        if(product.name.toUpperCase().indexOf(this.props.textFilter.toUpperCase()) > -1){
+          listaCategory[product.category].push(product);
+        }
+    });
+    Object.keys(listaCategory).map((key)=>{
+      if(listaCategory[key].length > 0)
+        listaProductos.push(<ProductCategoryRow key={key} name={key}/>);
+      listaCategory[key].forEach((item)=>{
+        if(!this.props.checkFilter || (this.props.checkFilter && item.stocked)){
+          listaProductos.push(<ProductRow key={item.name} name={item.name} price={item.price}/>);
+        }
+      });
+    });
+    return(
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaProductos}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class ProductRow extends React.Component{
+  render(){
+    return(
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{this.props.price}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductCategoryRow extends React.Component{
+  render(){
+    return(
+      <tr>
+        <th>
+          {this.props.name}
+        </th>
+      </tr>
+    );
+  }
+}
+
+export default FilterableProductTable;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import FilterableProductTable from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Weed', price: '$199.99', stocked: true, name: 'Imc Kush'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'XIAMOI'},
+    {category: 'Weed', price: '$199.99', stocked: true, name: 'Sandy Kush'}
+  ];
+
+ReactDOM.render(<FilterableProductTable products={PRODUCTS}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
