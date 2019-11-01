0ce4c7defbc37a04a07964410b55aca0ce4613c8
IMCBER <imcber1@gmail.com>
2019-11-01 01:15:05 -0600

    add home view

--- src/components/home/homeContainer.js
+++ src/components/home/homeContainer.js
@@ -1,24 +1,76 @@
 import React from 'react';
 import './homeContainer.css';
 
+const listSection = [
+    {img:"../",title:"Seed",summary:"lorem"},
+    {img:"../",title:"Flower",summary:"lorem"},
+    {img:"../",title:"Extract",summary:"lorem"}
+];
 class HomeContainer extends React.Component{
     render(){
         return(
             <section>
-                <TitleContainer />
+                <TitleContainer listSection={listSection}/>
             </section>
         );
     }
 }
 
 class TitleContainer extends React.Component{
-    render(){
-        return(
-            <section className='title-container'>
+    constructor(props){
+        super(props);
+        this.state = {
+            meetThemActive : false
+        };
+        this.clickMeetThem = this.clickMeetThem.bind(this);
+    }
 
+    clickMeetThem(e){
+        this.setState({
+            meetThemActive : !this.state.meetThemActive
+        });
+    }
+
+    render(){
+        var listSection = this.props.listSection;
+        var sectionOn = this.state.meetThemActive;
+        return(
+            <>
+            <section className='title-container'>
+                <div>
+                    <span className='title'>UN NUEVO CONCEPTO EN FLORES</span>
+                    <br/>
+                    <br/>
+                    <button onClick={this.clickMeetThem} className='btn-explore'>Conócelo</button>
+                    <br/>
+                    <div className="pd-1">
+                        {listSection.map((item) => <ImgSection key={item.title} section={item}/>)}
+                    </div>
+                </div>
             </section>
+            {sectionOn && <MeetThemView />}
+            </>
         );
     }
 }
 
+class ImgSection extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <img className="list-img-title" src={this.props.section.title}></img>
+        );
+    }
+}
+
+function MeetThemView(props) {
+    return(
+        <section>
+            <h1>HOLI!</h1>
+        </section>
+    );
+}
 export default HomeContainer;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import FilterableProductTable from './App';
 import HomeContainer from './components/home/homeContainer';
 import * as serviceWorker from './serviceWorker';
 
-const PRODUCTS = [
-    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Weed', price: '$199.99', stocked: true, name: 'Imc Kush'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'XIAMOI'},
-    {category: 'Weed', price: '$199.99', stocked: true, name: 'Sandy Kush'}
-  ];
 
 ReactDOM.render(<HomeContainer />, document.getElementById('root'));
 
