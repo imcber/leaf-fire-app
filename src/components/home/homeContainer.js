a9cfea5ce090d04328b4e2aa1afad62d49559280
IMCBER <imcber1@gmail.com>
2019-11-03 03:19:10 -0600

    add hooks and other functions

--- src/components/home/homeContainer.js
+++ src/components/home/homeContainer.js
@@ -1,86 +1,90 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './homeContainer.css';
 
-let iconSeed = require("../../assets/home/seed.png");
-let iconFlower = require("../../assets/home/flower.png");
-let iconExtract = require("../../assets/home/extract.png");
 let logo = require("../../assets/home/logo.png");
 let parallax_1 = require("../../assets/home/parallax-1.png");
 let parallax_2 = require("../../assets/home/parallax-2.png");
 
-const listSection = [
-    {img:iconSeed,title:"Seed",summary:"lorem",class:"list-img-title"},
-    {img:iconFlower,title:"Flower",summary:"lorem",class:"list-img-title pd-left-3"},
-    {img:iconExtract,title:"Extract",summary:"lorem",class:"list-img-title pd-left-3"}
-];
-class HomeContainer extends React.Component{
-    render(){
-        return(
-            <section>
-                <TitleContainer listSection={listSection}/>
-            </section>
-        );
-    }
+function HomeContainer(props){
+    return(
+        <section className="main-section">
+            <TitleContainer listSection={props.listSection}/>
+        </section>
+    );
 }
 
-class TitleContainer extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            meetThemActive : false
-        };
-        this.clickMeetThem = this.clickMeetThem.bind(this);
+function TitleContainer(props){
+    const [sectionOn,setSectionOn] = useState(false);
+    const [dataSectionView,setDataSectionView] = useState({});
+    
+    function changeSection(data,sectionOnInd){
+        setSectionOn(sectionOnInd);
+        setDataSectionView(data)
     }
-
-    clickMeetThem(e){
-        this.setState({
-            meetThemActive : !this.state.meetThemActive
-        });
-    }
-
-    render(){
-        var listSection = this.props.listSection;
-        var sectionOn = this.state.meetThemActive;
-        return(
-            <>
-            <section className='title-container'>
-                <img src={parallax_1}></img>
-                <div>
-                    <img class="logo-img" src={logo}></img>
-                    <br/>
-                    <span className='title'>UN NUEVO CONCEPTO EN FLORES</span>
-                    <br/>
-                    <br/>
-                    <button onClick={this.clickMeetThem} className='btn-explore'>Conócelo</button>
-                    <br/>
-                    <div className="pd-1">
-                        {listSection.map((item) => <ImgSection key={item.title} section={item}/>)}
+        
+    return(
+        <>
+            {!sectionOn && 
+                <section className='title-container'>
+                    <BackImg img={parallax_1}/>
+                    <BackImg img={parallax_2}/>
+                    <div className='elem-container'>
+                        <img className="logo-img" src={logo}></img>
+                        <br/>
+                        <span className='title'>UN NUEVO CONCEPTO EN FLORES</span>
+                        <br/>
+                        <br/>
+                        <button className='btn-explore'>Conócelo</button>
+                        <br/>
+                        <div className="pd-1">
+                            {props.listSection.map((item) => <ImgSection key={item.title} section={item}
+                                onChangeSection={changeSection}/>)}
+                        </div>
                     </div>
-                </div>
-            </section>
-            {sectionOn && <MeetThemView />}
-            </>
-        );
-    }
+                </section>
+            }
+            {!sectionOn ?<MeetThemView />:<SectionView dataSection={dataSectionView} onChangeSection={changeSection}/>}
+        </>
+    );
 }
 
-class ImgSection extends React.Component{
-    constructor(props){
-        super(props);
+function ImgSection(props){
+    function handleChange(){
+        props.onChangeSection(props.section,true);
     }
 
-    render(){
-        return(
-            <img className={this.props.section.class} src={this.props.section.img}></img>
-        );
-    }
+    return(
+        <a className={props.section.class}>
+            <img className='list-img-title' src={props.section.img} onClick={handleChange}/>
+        </a>
+    );
 }
+
 
 function MeetThemView(props) {
     return(
-        <section>
-            <h1>HOLI!</h1>
+        <section className='meet-them'>
+
         </section>
+    );
+}
+
+function BackImg(props) {
+    return(
+        <div className='back-container'>
+            <img className='back-img' src={props.img}></img>
+        </div>
+    );
+}
+
+function SectionView(props) {
+    
+    function returnHome() {
+        props.onChangeSection({},false)
+    }
+
+    return(
+        <h1 onClick={returnHome}>{props.dataSection.summary}</h1>
     );
 }
 export default HomeContainer;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import './index.css';
 import HomeContainer from './components/home/homeContainer';
 import * as serviceWorker from './serviceWorker';
 
+let iconSeed = require("./assets/home/seed.png");
+let iconFlower = require("./assets/home/flower.png");
+let iconExtract = require("./assets/home/extract.png");
 
-ReactDOM.render(<HomeContainer />, document.getElementById('root'));
+const listSection = [
+    {img:iconSeed,title:"Seed",summary:"lorem seed",class:""},
+    {img:iconFlower,title:"Flower",summary:"lorem flower",class:"pd-left-3"},
+    {img:iconExtract,title:"Extract",summary:"lorem extract",class:"pd-left-3"}
+];
+
+ReactDOM.render(<HomeContainer listSection={listSection}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
